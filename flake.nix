7baedf5153e23aee0fee183c6b1d863711be8f7f
Zarak <7254237+zarak@users.noreply.github.com>
2022-10-05 22:06:13 +0500

    Create dev shell with go binary

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.go = pkgs.go;
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ go ]; 
+        };
+      });
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zarak/personal-blog
+
+go 1.18
